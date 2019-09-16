f0aed36d65bbcb8ee48281ae9f134ec572bf9695
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-16 20:30:37 +0000

    es6

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 const assertEqual = function(actual, expected) {
   
   if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected)
+    console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected)
+    console.log("🛑🛑🛑 Assertion Failed: ${actual} === ${expected}")
   }
 
 
@@ -15,4 +15,4 @@ const assertEqual = function(actual, expected) {
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(1, 1)
