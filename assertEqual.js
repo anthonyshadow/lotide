672dc547af141d307212c921aa54a32b3de2b832
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-17 22:47:56 +0000

    pass and faill assertions for Arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,33 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
 
   if (actual === expected) {
     console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
   } else {
     console.log("🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}")
   }
+}
 
+const eqArrays = function (array1, array2) {
+  if (array1.length === array2.length) {
+    for (var i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+}
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1)
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    if (actual === expected) {
+      console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
+    } else {
+      console.log("🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}")
+    }
+  }
+}
+
