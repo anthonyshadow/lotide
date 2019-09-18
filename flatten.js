236fe13d519ca5097a0880e251a59934adc99005
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-18 00:08:28 +0000

    remove brackets of arrays within arrays to make one larger array

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
     for (var i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
@@ -11,7 +11,7 @@ const eqArrays = function(array1, array2) {
   return true;
 }
 
-const assertArraysEqual = function(actual, expected) {
+const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     if (actual === expected) {
       console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
@@ -21,3 +21,18 @@ const assertArraysEqual = function(actual, expected) {
   }
 }
 
+const flatten = function(arr) {
+  let merge = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let k = 0; k < arr[i].length; k++) {
+        merge.push(arr[i][k]);
+      }
+    } else {
+      merge.push(arr[i]);
+    }
+  }
+  return merge;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
