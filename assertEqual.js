64958441549d75bda50afb4afac4ee4241fd0103
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-18 16:53:29 +0000

    counting amount of items neccesary

--- assertEqual.js
+++ assertEqual.js
@@ -8,16 +8,3 @@ const assertEqual = function (actual, expected) {
   }
 }
 
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (var i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-}
-
