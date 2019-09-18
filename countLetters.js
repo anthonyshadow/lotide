0e9d36b343d5577841375c1e0a2b90c9db0ec759
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-18 17:19:38 +0000

    count amount of times a letter appears in a word

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}")
+  }
+}
+
+const countLetters = function(word) {
+  let results = {};
+  const removeSpaces = word.split(' ').join('').toLowerCase('');
+  for (const letter of removeSpaces) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters('Lighthouse in the House.'));
