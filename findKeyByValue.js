ed5c09ba5cbfcb51a92b6d1c0b7ef6ae12ee40f9
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-18 19:30:32 +0000

    Find the key of value by searching for a specific value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: ${actual} === ${expected}")
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}")
+  }
+}
+
+const findKeyByValue = function(list, value){
+  let keys = Object.keys(list);
+  for (let key of keys) {
+    if (list[key] === value) {
+      return key;
+    }
+  }
+}
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "family guy"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
